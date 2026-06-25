5a7dc788175ca943300af5164dd97ea5a61d6c73
nvnx7 <thenvnsahu@gmail.com>
2026-06-25 14:14:02 +0530

    feat: add utilities

--- crypto/src/utils.ts
+++ crypto/src/utils.ts
@@ -1,4 +1,12 @@
+import { bytesToNumberBE, randomBytes } from "@noble/curves/utils.js";
+import { ORDER } from "./constants";
+
 export function mod(a: bigint, n: bigint): bigint {
-    const res = a % n;
-    return res < 0n ? res + n : res;
+  const res = a % n;
+  return res < 0n ? res + n : res;
+}
+
+export function randomScalar(): bigint {
+  // 48 bytes before reducing → negligible modulo bias.
+  return mod(bytesToNumberBE(randomBytes(48)), ORDER);
 }
